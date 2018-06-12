bb439102060141298fcf8f7e52bad905e62716b0
kingcos <2821836721v@gmail.com>
2018-06-12 22:52:52 +0800

    Hide titleBar

--- Jirara/MainWindowController.swift
+++ Jirara/MainWindowController.swift
@@ -13,7 +13,9 @@ class MainWindowController: NSWindowController {
     override func windowDidLoad() {
         super.windowDidLoad()
     
-        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+        // 隐藏 TitleBar
+        window?.titleVisibility = .hidden
+        window?.titlebarAppearsTransparent = true
     }
 
 }
