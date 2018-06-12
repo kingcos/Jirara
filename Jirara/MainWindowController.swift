4c66cb97e65947f9b685fc3dd8fd832b64be2f08
kingcos <2821836721v@gmail.com>
2018-06-12 22:48:51 +0800

    Add MainWindowController

---
+++ Jirara/MainWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  MainWindowController.swift
+//  Jirara
+//
+//  Created by kingcos on 2018/6/12.
+//  Copyright © 2018 kingcos. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
