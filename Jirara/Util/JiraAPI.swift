1636c85492fc5f88a2386a1b48b85f90c066bb49
kingcos <2821836721v@gmail.com>
2018-07-11 21:27:06 +0800

    Add issue api

--- Jirara/Util/JiraAPI.swift
+++ Jirara/Util/JiraAPI.swift
@@ -12,11 +12,12 @@ enum JiraAPI: String {
     // 前缀
     case prefix = "https://"
     // 个人
-    case myself = "/rest/api/2/myself"
+//    case myself = "/rest/api/2/myself"
     case rapidView = "/rest/greenhopper/1.0/rapidview"
     case sprintQuery = "/rest/greenhopper/1.0/sprintquery/"
     case sprintReport = "/rest/greenhopper/1.0/rapid/charts/sprintreport"
     case userAvatar = "/secure/useravatar?ownerId="
-    case user = "/rest/api/2/user"
+    case user = "/rest/api/latest/user"
     case issueWeb = "/browse/"
+    case issue = "/rest/api/latest/issue/"
 }
