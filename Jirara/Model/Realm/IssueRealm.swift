9b3c07a27ce3aaaa1f3ae4f9e02596ccd0153f64
kingcos <2821836721v@gmail.com>
2018-07-19 18:02:30 +0800

    Removed comment

--- Jirara/Model/Realm/IssueRealm.swift
+++ Jirara/Model/Realm/IssueRealm.swift
@@ -23,34 +23,6 @@ class IssueRealm: Object {
     override static func primaryKey() -> String? {
         return "id"
     }
-    
-    override var description: String {
-        return ""
-//        var priority = ""
-//        var status = ""
-//
-//        switch priorityName {
-//        case "低优先级", "最低优先级": priority = "💚"
-//        case "默认优先级": priority = "💛"
-//        case "最高优先级(立刻执行)", "高优先级": priority = "❤️"
-//        default: priority = priorityName
-//        }
-//
-//        switch statusName {
-//        case "Start": status = "🏁 (\(statusName))"
-//        case "完成": status = "✅"
-//        default: status = statusName
-//        }
-//
-//        return
-//"""
-//<tr>
-//<td style="border:1px solid #B0B0B0"><a href="\(JiraAPI.prefix.rawValue + UserDefaults.get(by: .accountJiraDomain) + JiraAPI.issueWeb.rawValue + key)">\(summary)</a></td>
-//<td style="border:1px solid #B0B0B0">\(priority)</td>
-//<td style="border:1px solid #B0B0B0">\(status)</td>
-//</tr>
-//"""
-    }
 }
 
 extension IssueRealm {
