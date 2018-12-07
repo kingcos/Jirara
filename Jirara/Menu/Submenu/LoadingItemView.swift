63b40f8d14f79080d753a72afcb12355414cd769
kingcos <2821836721v@gmail.com>
2018-12-07 14:03:59 +0800

    Add loading item

---
+++ Jirara/Menu/Submenu/LoadingItemView.swift
@@ -0,0 +1,29 @@
+//
+//  LoadingItemView.swift
+//  Jirara
+//
+//  Created by kingcos on 2018/12/7.
+//  Copyright © 2018 kingcos. All rights reserved.
+//
+
+import Cocoa
+
+class LoadingItemView: NSView {
+    @IBOutlet weak var spinIndicator: NSProgressIndicator!
+    
+    class func load(_ owner: Any?) -> LoadingItemView? {
+        let ib = NSNib(nibNamed: .LoadingItemView, bundle: nil)
+        var arr: NSArray?
+        if !(ib?.instantiate(withOwner: owner, topLevelObjects: &arr) ?? false){
+            return nil
+        }
+        
+        for obj in arr ?? [] {
+            if let view = obj as? LoadingItemView {
+                return view
+            }
+        }
+        
+        return nil
+    }
+}
