d8eab6e19a31ead1acae050d4a25ab37f34e205f
kingcos <2821836721v@gmail.com>
2018-12-09 21:55:33 +0800

    Add scrums view model

---
+++ Jirara/ViewModel/ScrumsViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ScrumsViewModel.swift
+//  Jirara
+//
+//  Created by kingcos on 2018/12/9.
+//  Copyright © 2018 kingcos. All rights reserved.
+//
+
+import Foundation
+
+struct ScrumsViewModel {
+    
+}
